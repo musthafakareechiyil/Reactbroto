373e3d309aadb66776525dcd28c3b50509993fd9
musthafa <musthafakareechiyil@gmail.com>
2023-06-13 01:18:51 +0530

    first commit

---
+++ src/component/Header.js
@@ -0,0 +1,15 @@
+function Header (props){
+    return (
+        <div className="header">
+            <img alt = 'logo'src="https://th.bing.com/th/id/OIP.rfhS3MqqrB12IFMYgsLDcgHaHa?pid=ImgDet&rs=1"/>
+            <ul>
+                <li>Home</li>
+                <li>suppot</li>
+                <li>cart</li>
+                <li>About Us</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Header;
